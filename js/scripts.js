9605da38cfe93b4bce2de33bfc7cd894f41b9d08
ziqinzhao2024 <162967342+ziqinzhao2024@users.noreply.github.com>
2024-03-11 20:04:48 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function changeText(){
-    var textsArray = ["Text 1","Text 2", "Text 3", "Text 4", "Text 5"];
-    var number = getRandomNumberBetween(0,textsArray.length - 1);
+    var textsArray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"];
+    var number = getRandomNumberBetween(0, textsArray.length - 1);
     console.log("Index: ", number);
     document.getElementById("heading").innerHTML = textsArray[number];
 }
